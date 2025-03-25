2f3d59569d99d71437f6c9c8fbe5ab6c01f677ea
hupicol <leramoshchina@gmail.com>
2025-03-25 17:42:07 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -396,7 +396,7 @@ def stop_test(message):
     user_answers[message.chat.id] = []  # Очищаем ответы пользователя
     bot.send_message(
         message.chat.id,
-        'На этом пока все! Если захочешь узнать еще, то выбирай из кнопочек:)'
+        'На этом пока все! Если захочешь узнать еще, то начинай заново через Меню:)'
     )
 
 @bot.message_handler(func=lambda message: message.text == "Пройти тест заново")
